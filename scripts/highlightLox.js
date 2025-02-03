0a3863f9aaf7680f14f53fcb5fec983ce26756ca
peshala-prabhapoorna <peshalaprabhapoorna@gmail.com>
2025-02-03 21:27:27 +0530

    feat: add lox language definition to highlight.js
    To avoid parsing keywords as variables, keywords must be omitted from
    the variable parsing regex.

---
+++ scripts/highlightLox.js
@@ -0,0 +1,74 @@
+hljs.registerLanguage('lox', (hljs) => {
+    const KEYWORDS = {
+        keyword: 'class fun var for if else print return while super this or and',
+        literal: 'true false nil',
+        operator: 'and or',
+        built_in: 'print clock',
+    };
+
+    const VARIABLE = {
+        scope: 'variable',
+        match: /\b(?!(?:class|fun|var|for|if|else|print|return|while|super|this|or|and|true|false|nil|clock)\b)[a-zA-Z_]\w*\b/
+    };
+
+    const OPERATORS = {
+        scope: 'operator',
+        match: /[=+\-*/!&|<>^%~?,.:]|==|!=|<=|>=|\|\||&&/
+    };
+
+    const FUNCTION_DECLARATION = {
+        beginKeywords: 'fun',
+        end: /[{;]/,
+        excludeEnd: true,
+        contains: [
+            hljs.inherit(hljs.UNDERSCORE_TITLE_MODE, {
+                scope: 'title.function'
+            }),
+            {
+                begin: /\(/,
+                end: /\)/,
+                contains: [VARIABLE]
+            }
+        ]
+    };
+
+    const CLASS_DECLARATION = {
+        beginKeywords: 'class',
+        end: /[{;]/,
+        excludeEnd: true,
+        contains: [
+            hljs.UNDERSCORE_TITLE_MODE,
+            {
+                begin: /</,
+                end: />/,
+                contains: [hljs.UNDERSCORE_IDENT_RE]
+            }
+        ]
+    };
+
+    const PROPERTY_ACCESS = {
+        begin: /\./,
+        scope: 'operator',
+        starts: {
+            scope: 'property',
+            match: hljs.UNDERSCORE_IDENT_RE
+        }
+    };
+
+    return {
+        name: 'lox',
+        case_insensitive: false,
+        keywords: KEYWORDS,
+        contains: [
+            hljs.QUOTE_STRING_MODE,
+            hljs.NUMBER_MODE,
+            hljs.C_LINE_COMMENT_MODE,
+            hljs.C_BLOCK_COMMENT_MODE,
+            FUNCTION_DECLARATION,
+            CLASS_DECLARATION,
+            PROPERTY_ACCESS,
+            OPERATORS,
+            VARIABLE,
+        ]
+    };
+});
