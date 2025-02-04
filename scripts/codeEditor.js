510c2895ea411b95b2f341ac877b625aaba93822
peshala-prabhapoorna <peshalaprabhapoorna@gmail.com>
2025-02-04 20:46:37 +0530

    feat: finalize code editor styling

---
+++ scripts/codeEditor.js
@@ -0,0 +1,3 @@
+// set code input width to prevent resizing when content exceeds size
+const codeInput = document.querySelector('#code-input');
+codeInput.style.maxWidth = codeInput.offsetWidth + "px";
