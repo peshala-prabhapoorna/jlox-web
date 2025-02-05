cbcff1ab78cf72c937c0cc5d5ad950a6a119436f
peshala-prabhapoorna <peshalaprabhapoorna@gmail.com>
2025-02-05 17:48:04 +0530

    feat: add live syntax highlighting

--- scripts/codeEditor.js
+++ scripts/codeEditor.js
@@ -1,3 +1,95 @@
 // set code input width to prevent resizing when content exceeds size
 const codeInput = document.querySelector('#code-input');
 codeInput.style.maxWidth = codeInput.offsetWidth + "px";
+
+const loxCode = document.querySelector('#code-input code');
+loxCode.textContent = `class MyClass {
+    fun myFunction(var x) {
+        if (x > 10) {
+            print "Hello, world!";
+            var clock = 12;
+        }
+    }
+}`;
+highlight(loxCode);
+
+// prevent tab natigation when in code input
+loxCode.addEventListener('keydown', () => {
+    if (event.key === 'Tab') {
+        event.preventDefault();
+    }
+});
+
+// >>> Mutation observer
+//const config = {childList: true, subtree: true };
+//const observer = new MutationObserver(() => {
+//    console.log('change');
+//    highlight();
+//});
+//observer.observe(loxCode, config);
+// <<< Mutation observer
+
+loxCode.addEventListener('keyup', (event) => {
+    if (
+        event.key === 'Enter' ||
+        event.key === 'Tab' ||
+        event.key === 'ArrowDown' ||
+        event.key === 'ArrowUp' ||
+        event.key === 'ArrowLeft' ||
+        event.key === 'ArrowRight'
+    ) return;
+
+    handleHighlight(event);
+});
+
+function handleHighlight(event) {
+    const caretXY = getCaretPosition(event);
+    highlight(event.target);
+    createSelectionFromPoint(caretXY.x, caretXY.y, caretXY.x, caretXY.y);
+}
+
+function highlight(codeElement) {
+    const highlightedCode = hljs.highlight(codeElement.textContent, {language: 'lox'}).value;
+    codeElement.innerHTML = highlightedCode;
+}
+
+function getCaretPosition() {
+    const selection = document.getSelection();
+    const caretRect = selection.getRangeAt(0).getClientRects();
+
+    return {
+        x: caretRect[0].x,
+        y: caretRect[0].y,
+    };
+}
+
+function createSelectionFromPoint(startX, startY, endX, endY) {
+    const doc = document;
+    let start, end, range = null;
+    if (typeof doc.caretPositionFromPoint != "undefined") {
+        // chrome, firefox
+        start = doc.caretPositionFromPoint(startX, startY);
+        end = doc.caretPositionFromPoint(endX, endY);
+        range = doc.createRange();
+        range.setStart(start.offsetNode, start.offset);
+        range.setEnd(end.offsetNode, end.offset);
+    } else if (typeof doc.caretRangeFromPoint != "undefined") {
+        start = doc.caretRangeFromPoint(startX, startY);
+        end = doc.caretRangeFromPoint(endX, endY);
+        range = doc.createRange();
+        range.setStart(start.startContainer, start.startOffset);
+        range.setEnd(end.startContainer, end.startOffset);
+    }
+    if (range !== null && typeof window.getSelection != "undefined") {
+        const sel = window.getSelection();
+        sel.removeAllRanges();
+        sel.addRange(range);
+    } else if (typeof doc.body.createTextRange != "undefined") {
+        range = doc.body.createTextRange();
+        range.moveToPoint(startX, startY);
+        const endRange = range.duplicate();
+        endRange.moveToPoint(endX, endY);
+        range.setEndPoint("EndToEnd", endRange);
+        range.select();
+    }
+}
